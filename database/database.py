2b2c89cbce01d35172c305a4258b0b7eb876392c
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2024-03-25 13:28:59 +0100

    create database maze_bank and create the connection from the database on the script

---
+++ database/database.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+class Database:
+    def __init__(self):
+        self.base = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "azerty",
+        database = "maze_bank",
+        )
+        self.cursor = self.base.cursor()
+
+    def execute_sql(self, query, params):
+        self.cursor.execute(query, params or ())
+        self.base.commit()
+
+    def fetch_all(self, query, params):
+        self.cursor.execute(query, params or ())
+        return self.cursor.fetchall()
+    
+    def fetch_one(self, query, params):
+        self.cursor.execute(query, params or ())
+        return self.cursor.fetchone()
+    
+    def closing_connection(self):
+        self.base.close()
---
+++ database/sql/create.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE maze_bank;
+
+USE maze_bank;
+
+CREATE TABLE user(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR (255),
+    firstname VARCHAR (255),
+    email VARCHAR (255) UNIQUE CHECK (REGEXP_LIKE(email, '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$')),
+    password VARCHAR (200)
+);
+
+CREATE TABLE transaction(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    amount INT,
+    heure DATETIME DEFAULT CURRENT_TIMESTAMP,
+    motif VARCHAR (255) 
+);
