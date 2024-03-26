ca8a136f47ec94bbc36e825e71845ec86495d5e7
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2024-03-26 10:53:59 +0100

    method for recovery email and password on database

---
+++ database/sql_manager.py
@@ -0,0 +1,20 @@
+from database.database import Database
+
+class SqlManager(Database):
+    def __init__(self):
+        Database.__init__(self)
+        
+    def retreview_email(self):
+        sql = "SELECT email FROM user"
+        self.fetch_all(sql, ())
+        
+    def retreview_password(self):
+        sql = "SELECT password FROM user"
+        self.fetch_all(sql, ())
+    
+    
+    
+    
+    
+    
+    
